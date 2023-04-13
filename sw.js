17ca3bab913a7016c4857ca4f7b52410d3582239
JulianAlejandroBaylon <72962251+JulianAlejandroBaylon@users.noreply.github.com>
2023-04-13 17:02:04 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,46 +13,50 @@ var urlsToCache = [
     '/windows11/Square44x44Logo.targetsize-256.png'
 ];
 
-self.addEventListener('install', event => { 
-    event.waitUntil(caches.open(CACHE_NAME)
-    .then(cache => { 
-        return cache
-        .addAll(urlsToCache)
-        .then(() => { 
-            self.skipWaiting() 
-        })
-    .catch(err => console.log('Hubo un error', err)) })); });
-
-self.addEventListener('activate', e => {
-    const cacheWhitelist = [CACHE_NAME];
-
-
+self.addEventListener("install", (e) => {
     e.waitUntil(
-        caches.keys()
-            .then(cacheNames => {
-                return Promise.all(
-                    cacheNames.map(cacheNames => {
-                        if (cacheWhitelist.indexOf(cacheNames) == -1) {
-                            return cache.delete(cacheNames);
-                        }
-                    })
-                );
-            })
-            .then(() => {
-                self.clients.claim();
-            })
+      caches
+        .open(cache_NAME)
+        .then((cache) => {
+          return cache.addAll(urlsToCache).then(() => {
+            self.skipWaiting();
+          });
+        })
+        .catch((err) => console.log("No se ha registrado el cache"), err)
     );
-
-});
-
-self.addEventListener('fetch', e => {
+  });
+  
+  //Event activate
+  self.addEventListener("activate", (e) => {
+    const cacheWhiteList = [cache_NAME];
+    
+    e.waitUntil(
+      caches.keys()
+        .then((cacheNames) => {
+          return Promise.all(
+            cacheNames.map((cacheNames) => {
+              if (cacheWhiteList.indexOf(cacheNames) == -1) {
+                return cache.delete(cacheNames);
+              }
+            })
+          );
+        })
+        .then(() => {
+          self.clients.claim();
+        })
+    );
+  });
+  
+  
+  //Event fetch
+  self.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches.match(e.request)
-            .then(res => {
-                if (res) {
-                    return res;
-                }
-                return fetch(e.request);
-            })
+      caches.match(e.request)
+      .then(res => {
+        if (res) {
+          return res;
+        }
+        return fetch(e.request);
+      })
     );
-});
+  });
