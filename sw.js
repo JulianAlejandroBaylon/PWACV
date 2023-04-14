a3abd07bec3e19ebf18d4818d394e23b0614c0c9
JulianAlejandroBaylon <72962251+JulianAlejandroBaylon@users.noreply.github.com>
2023-04-14 17:34:02 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //Asignar nombre y version de la cache
-const CACHE_NAME = 'v1_cache_JBN_PWA';
+const cache_NAME = 'v1_cache_JBN_PWA';
 
 //configuracion de los ficheros
 var urlsToCache = [
@@ -13,44 +13,50 @@ var urlsToCache = [
     '/windows11/Square44x44Logo.targetsize-256.png'
 ];
 
-// Instala el Service Worker y almacena los archivos en cache
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache abierta');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Intercepta las solicitudes de red y devuelve los archivos almacenados en cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Devuelve la respuesta almacenada en cache, si existe
-        if (response) {
-          return response;
-        }
-        // Si no, solicita la respuesta a la red
-        return fetch(event.request);
-      })
-  );
-});
-
-// Elimina caches antiguas
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['my-pwa-cache'];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+      caches
+        .open(cache_NAME)
+        .then((cache) => {
+          return cache.addAll(urlsToCache).then(() => {
+            self.skipWaiting();
+          });
         })
-      );
-    })
-  );
-});
+        .catch((err) => console.log("No se ha registrado el cache"), err)
+    );
+  });
+  
+  //Event activate
+  self.addEventListener("activate", (e) => {
+    const cacheWhiteList = [cache_NAME];
+    
+    e.waitUntil(
+      caches.keys()
+        .then((cacheNames) => {
+          return Promise.all(
+            cacheNames.map((cacheNames) => {
+              if (cacheWhiteList.indexOf(cacheNames) == -1) {
+                return cache.delete(cacheNames);
+              }
+            })
+          );
+        })
+        .then(() => {
+          self.clients.claim();
+        })
+    );
+  });
+  
+  
+  //Event fetch
+  self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches.match(e.request)
+      .then(res => {
+        if (res) {
+          return res;
+        }
+        return fetch(e.request);
+      })
+    );
+  });
